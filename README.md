964c77c6002663aebd7de8c8b1d7172b09e5d25d
Kris Torres <torres-kris@outlook.com>
2023-11-08 11:36:04 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Trebek
+======
+
+<p>
+    <img src="https://img.shields.io/badge/Svelte-3-ff3e00?style=for-the-badge&logo=svelte" alt="" />
+</p>
+
+A *Jeopardy!* game board interface used at (virtual) office happy hours, trivia
+nights, or other fun events.
+
+Building and Running Locally
+----------------------------
+
+```sh
+git clone https://github.com/kristorres/trebek
+cd trebek
+pnpm install
+pnpm build/dev
+```
---
+++ src/app.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {wsx} from "@axel669/zephyr"
+</script>
+
+<style>
+    :global([ws-x~="theme[dark]"]) {
+        --trebek-blue: #081476;
+        --trebek-gold: #fba61c;
+
+        --primary: #4955d8;
+        --primary-ripple: #4955d860;
+        --secondary: var(--torres-gold);
+        --secondary-ripple: #fba61c60;
+        --background: #121319;
+        --background-layer: #323339;
+
+        --text-light: #ffffffc0;
+        --text-color-secondary: #ffffff80;
+    }
+</style>
+
+<svelte:body use:wsx={{theme: "dark", "@app": true}} />
